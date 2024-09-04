cea9fc6c344d30f1cfeb4c9236e37bbccf0eee10
GBuard <guenolebuard.pro@gmail.com>
2024-09-04 09:35:53 +0200

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,19 +1,29 @@
 import memojiImage from '@/assets/images/memoji-computer.png';
 import Image from 'next/image';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="py-32">
       <div className="container">
-        <Image src={memojiImage} alt="Memoji on computer"/>
-        <div>
+        <div className="flex flex-col items-center">
+        <Image src={memojiImage} className="size-[100px]" alt="Memoji on computer"/>
+        <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex">
           <div></div>
           <div>Disponible pour de nouveaux projets</div>
+        </div>
         </div>
         <h1>Développeur Front-End exceptionnel</h1>
         <p>Je suis spécialisé dans la transformation de designs en application web performante et optimisée. Parlons de votre prochain projet ensemble.</p>
         <div>
-          <button></button>
+          <button>
+            <span>Explorez mes travaux</span>
+            <ArrowDown/>
+          </button>
+          <button>
+            <span>👋</span>
+            <span>Connectons nous</span>
+          </button>
         </div>
       </div>
   </div>
