be0d3a66da0334fddf847490010a5d2dd0727530
GBuard <guenolebuard.pro@gmail.com>
2024-09-26 16:15:58 +0200

    Responsive section part-1

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -47,28 +47,29 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
     return (
-        <section className="pb-16">
+        <section className="pb-16 lg:py-24">
             <div className="container">
                 <div className="flex justify-center">
                     <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
                         Real-world Results
                     </p>
                 </div>
-                <h2 className="font-serif text-3xl text-center mt-6">
+                <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
                     Featured Projects
                 </h2>
-                <p className="text-center text-white/60 mt-4">
-                    Regardez comment j&apos;ai transformé des concepts en
+                <p className="text-center md:text-lg lg:text-lg text-white/60 mt-4 max-w-md mx-auto">
+                    Découvrez comment j&apos;ai transformé des concepts en
                     expériences numériques attractives.
                 </p>
-                <div className="flex flex-col mt-10 gap-20">
+                <div className="mt-10 md:mt-20 flex flex-col gap-20">
                     {portfolioProjects.map((project) => (
                         <div
                             key={project.title}
                             className="bg-gray-800 rounded-3xl relative z-0
                             overflow-hidden after:z-10 after:content-[''] after:absolute
                             after:inset-0 after:outline-2 after:outline
-                            after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8"
+                            after:-outline-offset-2 after:rounded-3xl after:outline-white/20
+                             px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none"
                         >
                             <div
                                 className="absolute inset-0 -z-10 opacity-5"
@@ -85,23 +86,26 @@ export const ProjectsSection = () => {
                                 <span>{project.year}</span>
                             </div>
 
-                            <h3 className="font-serif text-2xl mt-2">
+                            <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
                                 {project.title}
                             </h3>
-                            <hr className="border-t-2 border-white/5 mt-4" />
-                            <ul className="flex flex-col gap-4 mt-4">
+                            <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                            <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                                 {project.results.map((result, index) => (
                                     <li
                                         key={index}
-                                        className="flex gap-2 text-sm text-white/50"
+                                        className="flex gap-2 text-sm md:text-base text-white/50"
                                     >
-                                        <CheckCircleIcon className="size-5" />
+                                        <CheckCircleIcon className="size-5 md:size-6" />
                                         <span>{result.title}</span>
                                     </li>
                                 ))}
                             </ul>
                             <a href={project.link}>
-                                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                                <button
+                                    className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl
+                                 font-semibold inline-flex items-center justify-center gap-2 mt-8"
+                                >
                                     <span>Visiter le site</span>
                                     <ArrowUpRightIcon className="size-4" />
                                 </button>
@@ -109,7 +113,7 @@ export const ProjectsSection = () => {
                             <Image
                                 src={project.image}
                                 alt={project.title}
-                                className="mt-8 -mb-4"
+                                className="mt-8 -mb-4 md:-mb-0"
                             />
                         </div>
                     ))}
